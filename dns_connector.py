688f6390e98a3349f7a5bf09eeaf11a0c5d263a2
Ryan Means <ryan.means@LT-AM2TP0KPFVH4.local>
2017-08-03 16:12:07 -0700

    PAPP-2956 PAPP-2669 Fix black on purple widget, update dependency to wheel

--- dns_connector.py
+++ dns_connector.py
@@ -20,8 +20,6 @@ import os
 
 # Imports local to this App
 from dns_consts import *
-app_dir = os.path.dirname(os.path.abspath(__file__))
-os.sys.path.insert(0, os.path.join(app_dir, 'dependencies/dnspython'))
 import dns.resolver as resolver  # noqa
 import dns.reversename as reversename  # noqa
 import ipaddr  # noqa
